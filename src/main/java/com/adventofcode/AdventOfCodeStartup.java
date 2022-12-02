098e76563e7e734594bb90644dceb5ba8648d3a2
OptiPanda <a.jamain.pro@gmail.com>
2022-12-02 11:54:08 +0100

    [Day1] Answers for Day 1

---
+++ src/main/java/com/adventofcode/AdventOfCodeStartup.java
@@ -0,0 +1,11 @@
+package com.adventofcode;
+
+public class AdventOfCodeStartup {
+
+    public static void main(String[] args) {
+
+        // Day 1
+        System.out.println("Day 1 - 1 : " + Day1.answer1());
+        System.out.println("Day 1 - 2 : " + Day1.answer2());
+    }
+}
