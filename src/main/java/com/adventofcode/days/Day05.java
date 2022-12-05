4669a4e0ca45d7d8070dd383050508694e66a75c
Arthur Jamain <a.jamain.pro@gmail.com>
2022-12-05 20:00:46 +0100

    [Day5] Answer 1 & Test

--- src/main/java/com/adventofcode/days/Day05.java
+++ src/main/java/com/adventofcode/days/Day05.java
@@ -3,19 +3,49 @@ package com.adventofcode.days;
 import com.adventofcode.utils.AdventOfCodeUtils;
 
 import java.lang.invoke.MethodHandles;
-import java.util.Scanner;
+import java.util.*;
 
 public class Day05 {
 
     private static final String FILE_PATH = "src/main/resources/input-" + MethodHandles.lookup().lookupClass().getSimpleName().toLowerCase();
 
-    public static Integer answer1() {
+    public static String answer1() {
 
         Scanner sc = AdventOfCodeUtils.getScanner(FILE_PATH);
 
-        int score = 0;
+        Map<Integer, List<String>> stock = new HashMap<>();
 
-        return score;
+        stock.put(1, new ArrayList<>(List.of("L","N","W","T","D")));
+        stock.put(2, new ArrayList<>(List.of("C","P","H")));
+        stock.put(3, new ArrayList<>(List.of("W","P","H","N","D","G","M","J")));
+        stock.put(4, new ArrayList<>(List.of("C","W","S","N","T","Q","L")));
+        stock.put(5, new ArrayList<>(List.of("P","H","C","N")));
+        stock.put(6, new ArrayList<>(List.of("T","H","N","D","M","W","Q","B")));
+        stock.put(7, new ArrayList<>(List.of("M","B","R","J","G","S","L")));
+        stock.put(8, new ArrayList<>(List.of("Z","N","W","G","V","B","R","T")));
+        stock.put(9, new ArrayList<>(List.of("W","G","D","N","P","L")));
+
+        int initRowsCount = 10;
+
+        while (initRowsCount-- > 0) {
+            sc.nextLine();
+        }
+
+        while (sc.hasNextLine()) {
+            Order order = getOrder(sc.nextLine());
+
+            for (int i = 0; i < order.move; i++) {
+
+                stock.get(order.to).add(stock.get(order.from).remove(stock.get(order.from).size() - 1));
+
+            }
+        }
+
+        StringBuilder answer = new StringBuilder();
+
+        stock.values().forEach(list -> answer.append(list.get(list.size()-1)));
+
+        return answer.toString();
     }
 
     public static Integer answer2() {
@@ -25,5 +55,27 @@ public class Day05 {
         int score = 0;
 
         return score;
+    }
+
+    public static Order getOrder(String line) {
+        line = line.replace("move ", "");
+        line = line.replace(" from ",";");
+        line = line.replace(" to ",";");
+
+        final String[] ordSplit = line.split(";");
+
+        return new Order(Integer.parseInt(ordSplit[0]),Integer.parseInt(ordSplit[1]),Integer.parseInt(ordSplit[2]));
+    }
+}
+
+class Order {
+    Integer move;
+    Integer from;
+    Integer to;
+
+    public Order(Integer move, Integer from, Integer to) {
+        this.move = move;
+        this.from = from;
+        this.to = to;
     }
 }
--- src/test/java/com/adventofcode/days/Day05Test.java
+++ src/test/java/com/adventofcode/days/Day05Test.java
@@ -8,7 +8,7 @@ public class Day05Test {
         System.out.println(CLASSNAME + " : Start tests");
         System.out.println("---");
 
-        boolean isOk = testAnswer1() & testAnswer2();
+        boolean isOk = testGetOrder() & testAnswer1() & testAnswer2();
 
         System.out.println("---");
 
@@ -16,9 +16,9 @@ public class Day05Test {
     }
 
     private static boolean testAnswer1() {
-        final Integer answer = Day05.answer1();
-        int expectedAnswer = 0;
-        if (answer != expectedAnswer) {
+        final String answer = Day05.answer1();
+        String expectedAnswer = "TWSGQHNHL";
+        if (!answer.equals(expectedAnswer)) {
             System.err.println(CLASSNAME + " answer 1 KO | given " + answer + " expected " + expectedAnswer);
             return false;
         }
@@ -34,6 +34,24 @@ public class Day05Test {
             return false;
         }
         System.out.println(CLASSNAME + " answer 2 OK");
+        return true;
+    }
+
+    private static boolean testGetOrder() {
+        final Order answer = Day05.getOrder("move 4 from 5 to 6");
+        if (answer.move != 4) {
+            System.err.println(CLASSNAME + " testGetOrder KO | given 4 expected 4 got" + answer.move);
+            return false;
+        }
+        if (answer.from != 5) {
+            System.err.println(CLASSNAME + " testGetOrder KO | given 5 expected 5 got" + answer.from);
+            return false;
+        }
+        if (answer.to != 6) {
+            System.err.println(CLASSNAME + " testGetOrder KO | given 6 expected 6 got" + answer.to);
+            return false;
+        }
+        System.out.println(CLASSNAME + " testGetOrder OK");
         return true;
     }
 }
