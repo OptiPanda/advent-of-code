ab25404075602e6e8bbe801dd16d0549264bebe5
OptiPanda <a.jamain.pro@gmail.com>
2022-12-08 10:06:29 +0100

    [2022][Day8] Answer 1 & Test

--- src/main/java/com/adventofcode/year2022/days/Day08.java
+++ src/main/java/com/adventofcode/year2022/days/Day08.java
@@ -12,8 +12,73 @@ public class Day08 {
     public static Integer answer1() {
 
         final Scanner sc = AdventOfCodeUtils.getScanner(FILE_PATH);
+        
+        Integer[][] map = new Integer[99][99];
 
-        return 0;
+        for (int i = 0; i < 99; i++) {
+            final String[] split = sc.nextLine().split("");
+            for (int j = 0; j < split.length; j++) {
+                map[i][j] = Integer.parseInt(split[j]);
+            }
+        }
+
+        int visible = 0;
+
+        for (int l = 0; l < map.length; l++) { // line
+
+            for (int c = 0; c < map[l].length; c++) { // column
+
+                int height = map[l][c];
+
+                int x = l;
+                int y = c;
+
+                boolean isVisibleLeft = true;
+
+                while (--y >= 0 && isVisibleLeft) {
+                    if (map[x][y] >= height) {
+                        isVisibleLeft = false;
+                    }
+                }
+
+                y = c;
+
+                boolean isVisibleRight = true;
+
+                while (++y < 99 && isVisibleRight) {
+                    if (map[x][y] >= height) {
+                        isVisibleRight = false;
+                    }
+                }
+
+                y = c;
+
+                boolean isVisibleTop = true;
+
+                while (--x >= 0 && isVisibleTop) {
+                    if (map[x][y] >= height) {
+                        isVisibleTop = false;
+                    }
+                }
+
+                x = l;
+
+                boolean isVisibleBottom = true;
+
+                while (++x < 99 && isVisibleBottom) {
+                    if (map[x][y] >= height) {
+                        isVisibleBottom = false;
+                    }
+                }
+
+                if(isVisibleLeft || isVisibleRight || isVisibleTop || isVisibleBottom) {
+                    visible++;
+                }
+
+            }
+        }
+
+        return visible;
     }
 
     public static Integer answer2() {
--- src/test/java/com/adventofcode/year2022/days/Day08Test.java
+++ src/test/java/com/adventofcode/year2022/days/Day08Test.java
@@ -19,7 +19,7 @@ public class Day08Test extends DayTest {
 
     private static boolean testAnswer1() {
         final Integer answer = Day08.answer1();
-        int expectedAnswer = 0;
+        int expectedAnswer = 1835;
         if (answer != expectedAnswer) {
             System.err.println(CLASSNAME + " answer 1 KO | given " + answer + " expected " + expectedAnswer);
             return false;
