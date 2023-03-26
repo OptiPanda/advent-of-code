c63795ff2d073d810bd14a04472502c8793dcbdd
Arthur Jamain <a.jamain.pro@gmail.com>
2023-03-26 13:08:53 +0200

    [2022][Day9] Answer 1 & Tests

--- src/main/java/com/adventofcode/year2022/days/Day09.java
+++ src/main/java/com/adventofcode/year2022/days/Day09.java
@@ -3,7 +3,9 @@ package com.adventofcode.year2022.days;
 import com.adventofcode.utils.AdventOfCodeUtils;
 
 import java.lang.invoke.MethodHandles;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Day09 {
 
@@ -13,7 +15,83 @@ public class Day09 {
 
         final Scanner sc = AdventOfCodeUtils.getScanner(FILE_PATH);
 
-        return 0;
+        int[] head = {0, 0};
+        int[] tail = {0, 0};
+
+        Set<String> histTpos = new HashSet<>();
+        histTpos.add(tail[0] + ";" + tail[1]);
+
+        while (sc.hasNextLine()) {
+            final String[] order = sc.nextLine().split(" ");
+
+
+            for (int i = 0; i < Integer.parseInt(order[1]); i++) {
+
+                switch (order[0]) {
+                    case "L" -> head[0]--;
+                    case "R" -> head[0]++;
+                    case "U" -> head[1]++;
+                    case "D" -> head[1]--;
+                }
+
+                tailFollowHead(head, tail);
+
+                histTpos.add(tail[0] + ";" + tail[1]);
+            }
+        }
+
+        return histTpos.size();
+    }
+
+    protected static void tailFollowHead(int[] head, int[] tail) {
+        if (head[0] - tail[0] > 1) {
+            tail[0]++;
+            if (head[1] - tail[1] == 1) {
+                tail[1]++;
+            } else if (tail[1] - head[1] == 1) {
+                tail[1]--;
+            }
+        } else if (tail[0] - head[0] > 1) {
+            tail[0]--;
+            if (head[1] - tail[1] == 1) {
+                tail[1]++;
+            } else if (tail[1] - head[1] == 1) {
+                tail[1]--;
+            }
+        }
+
+        if (head[1] - tail[1] > 1) {
+            tail[1]++;
+            if (head[0] - tail[0] == 1) {
+                tail[0]++;
+            } else if (tail[0] - head[0] == 1) {
+                tail[0]--;
+            }
+        } else if (tail[1] - head[1] > 1) {
+            tail[1]--;
+            if (head[0] - tail[0] == 1) {
+                tail[0]++;
+            } else if (tail[0] - head[0] == 1) {
+                tail[0]--;
+            }
+        }
+    }
+
+    protected static void print(int[] head, int[] tail) {
+        System.out.println("H[" + head[0] + "," + head[1] + "] -> T[" + tail[0] + "," + tail[1] + "]");
+        for (int i = 5; i > -5; i--) {
+            for (int j = -5; j < 5; j++) {
+                if (head[0] % 5 == i && head[1] % 5 == j) {
+                    System.out.print("H");
+                } else if (tail[0] % 5 == i && tail[1] % 5 == j) {
+                    System.out.print("T");
+                } else {
+                    System.out.print(".");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
 
     public static Integer answer2() {
--- src/test/java/com/adventofcode/year2022/days/Day09Test.java
+++ src/test/java/com/adventofcode/year2022/days/Day09Test.java
@@ -10,7 +10,10 @@ public class Day09Test extends DayTest {
         System.out.println(CLASSNAME + " : Start tests");
         System.out.println("---");
 
-        boolean isOk = testAnswer1() & testAnswer2();
+        boolean isOk = testTailFollowHead()
+                && testAnswer1()
+                && testAnswer2()
+                ;
 
         System.out.println("---");
 
@@ -19,7 +22,7 @@ public class Day09Test extends DayTest {
 
     private static boolean testAnswer1() {
         final Integer answer = Day09.answer1();
-        int expectedAnswer = 0;
+        int expectedAnswer = 6256;
         if (answer != expectedAnswer) {
             System.err.println(CLASSNAME + " answer 1 KO | given " + answer + " expected " + expectedAnswer);
             return false;
@@ -36,6 +39,263 @@ public class Day09Test extends DayTest {
             return false;
         }
         System.out.println(CLASSNAME + " answer 2 OK");
+        return true;
+    }
+
+    private static boolean testTailFollowHead() {
+        return testUp()
+            && testRight()
+            && testLeft()
+            && testDown()
+            && testLeftDown()
+            && testRightDown()
+            && testLeftUp()
+            && testRightUp()
+            && testRightUp2()
+            && testNoMove()
+            && testNoMove2()
+        ;
+    }
+
+    /**
+     * #####    #####
+     * #####    #####
+     * ##H## -> ##H##
+     * #####    ##T##
+     * ##T##    #####
+     */
+    private static boolean testUp() {
+        int[] head = {0, 0};
+        int[] tail = {-2, 0};
+
+        Day09.tailFollowHead(head, tail);
+
+        int[] expectedAnswer = {-1, 0};
+        if (tail[0] != expectedAnswer[0] || tail[1] != expectedAnswer[1]) {
+            System.err.println(CLASSNAME + " testUp KO | given [" + tail[0] + "," + tail[1] + "] expected [" + expectedAnswer[0] + "," + expectedAnswer[1] + "]");
+            return false;
+        }
+        System.out.println(CLASSNAME + " testUp OK");
+        return true;
+    }
+
+    /**
+     * ##T##    #####
+     * #####    ##T##
+     * ##H## -> ##H##
+     * #####    #####
+     * #####    #####
+     */
+    private static boolean testDown() {
+        int[] head = {0, 0};
+        int[] tail = {2, 0};
+
+        Day09.tailFollowHead(head, tail);
+
+        int[] expectedAnswer = {1, 0};
+        if (tail[0] != expectedAnswer[0] || tail[1] != expectedAnswer[1]) {
+            System.err.println(CLASSNAME + " testDown KO | given [" + tail[0] + "," + tail[1] + "] expected [" + expectedAnswer[0] + "," + expectedAnswer[1] + "]");
+            return false;
+        }
+        System.out.println(CLASSNAME + " testDown OK");
+        return true;
+    }
+
+    /**
+     * #####    #####
+     * #####    #####
+     * ##H#T -> ##HT#
+     * #####    #####
+     * #####    #####
+     */
+    private static boolean testLeft() {
+        int[] head = {0, 0};
+        int[] tail = {0, 2};
+
+        Day09.tailFollowHead(head, tail);
+
+        int[] expectedAnswer = {0, 1};
+        if (tail[0] != expectedAnswer[0] || tail[1] != expectedAnswer[1]) {
+            System.err.println(CLASSNAME + " testLeft KO | given [" + tail[0] + "," + tail[1] + "] expected [" + expectedAnswer[0] + "," + expectedAnswer[1] + "]");
+            return false;
+        }
+        System.out.println(CLASSNAME + " testLeft OK");
+        return true;
+    }
+
+    /**
+     * #####    #####
+     * #####    #####
+     * T#H## -> #TH##
+     * #####    #####
+     * #####    #####
+     */
+    private static boolean testRight() {
+        int[] head = {0, 0};
+        int[] tail = {0, -2};
+
+        Day09.tailFollowHead(head, tail);
+
+        int[] expectedAnswer = {0, -1};
+        if (tail[0] != expectedAnswer[0] || tail[1] != expectedAnswer[1]) {
+            System.err.println(CLASSNAME + " testRight KO | given [" + tail[0] + "," + tail[1] + "] expected [" + expectedAnswer[0] + "," + expectedAnswer[1] + "]");
+            return false;
+        }
+        System.out.println(CLASSNAME + " testRight OK");
+        return true;
+    }
+
+    /**
+     * ###T#    #####
+     * #####    ##T##
+     * ##H## -> ##H##
+     * #####    #####
+     * #####    #####
+     */
+    private static boolean testLeftDown() {
+        int[] head = {0, 0};
+        int[] tail = {2, 1};
+
+        Day09.tailFollowHead(head, tail);
+
+        int[] expectedAnswer = {1, 0};
+        if (tail[0] != expectedAnswer[0] || tail[1] != expectedAnswer[1]) {
+            System.err.println(CLASSNAME + " testLeftDown KO | given [" + tail[0] + "," + tail[1] + "] expected [" + expectedAnswer[0] + "," + expectedAnswer[1] + "]");
+            return false;
+        }
+        System.out.println(CLASSNAME + " testLeftDown OK");
+        return true;
+    }
+
+    /**
+     * #T###    #####
+     * #####    ##T##
+     * ##H## -> ##H##
+     * #####    #####
+     * #####    #####
+     */
+    private static boolean testRightDown() {
+        int[] head = {0, 0};
+        int[] tail = {2, -1};
+
+        Day09.tailFollowHead(head, tail);
+
+        int[] expectedAnswer = {1, 0};
+        if (tail[0] != expectedAnswer[0] || tail[1] != expectedAnswer[1]) {
+            System.err.println(CLASSNAME + " testRightDown KO | given [" + tail[0] + "," + tail[1] + "] expected [" + expectedAnswer[0] + "," + expectedAnswer[1] + "]");
+            return false;
+        }
+        System.out.println(CLASSNAME + " testRightDown OK");
+        return true;
+    }
+
+    /**
+     * #####    #####
+     * #####    #####
+     * ##H## -> ##H##
+     * #####    ##T##
+     * ###T#    #####
+     */
+    private static boolean testLeftUp() {
+        int[] head = {0, 0};
+        int[] tail = {-2, 1};
+
+        Day09.tailFollowHead(head, tail);
+
+        int[] expectedAnswer = {-1, 0};
+        if (tail[0] != expectedAnswer[0] || tail[1] != expectedAnswer[1]) {
+            System.err.println(CLASSNAME + " testLeftUp KO | given [" + tail[0] + "," + tail[1] + "] expected [" + expectedAnswer[0] + "," + expectedAnswer[1] + "]");
+            return false;
+        }
+        System.out.println(CLASSNAME + " testLeftUp OK");
+        return true;
+    }
+
+    /**
+     * #####    #####
+     * #####    #####
+     * ##H## -> ##H##
+     * #####    ##T##
+     * #T###    #####
+     */
+    private static boolean testRightUp() {
+        int[] head = {0, 0};
+        int[] tail = {-2, -1};
+
+        Day09.tailFollowHead(head, tail);
+
+        int[] expectedAnswer = {-1, 0};
+        if (tail[0] != expectedAnswer[0] || tail[1] != expectedAnswer[1]) {
+            System.err.println(CLASSNAME + " testRightUp KO | given [" + tail[0] + "," + tail[1] + "] expected [" + expectedAnswer[0] + "," + expectedAnswer[1] + "]");
+            return false;
+        }
+        System.out.println(CLASSNAME + " testRightUp OK");
+        return true;
+    }
+
+    /**
+     * #####    #####
+     * #####    #####
+     * ##H## -> #TH##
+     * T####    #####
+     * #####    #####
+     */
+    private static boolean testRightUp2() {
+        int[] head = {0, 0};
+        int[] tail = {-2, -1};
+
+        Day09.tailFollowHead(head, tail);
+
+        int[] expectedAnswer = {-1, 0};
+        if (tail[0] != expectedAnswer[0] || tail[1] != expectedAnswer[1]) {
+            System.err.println(CLASSNAME + " testRightUp3 KO | given [" + tail[0] + "," + tail[1] + "] expected [" + expectedAnswer[0] + "," + expectedAnswer[1] + "]");
+            return false;
+        }
+        System.out.println(CLASSNAME + " testRightUp3 OK");
+        return true;
+    }
+
+    /**
+     * #####    #####
+     * #####    #####
+     * ##X## -> ##X##
+     * #####    #####
+     * #####    #####
+     */
+    private static boolean testNoMove() {
+        int[] head = {0, 0};
+        int[] tail = {0, 0};
+
+        Day09.tailFollowHead(head, tail);
+
+        int[] expectedAnswer = {0, 0};
+        if (tail[0] != expectedAnswer[0] || tail[1] != expectedAnswer[1]) {
+            System.err.println(CLASSNAME + " testNoMove KO | given [" + tail[0] + "," + tail[1] + "] expected [" + expectedAnswer[0] + "," + expectedAnswer[1] + "]");
+            return false;
+        }
+        System.out.println(CLASSNAME + " testNoMove OK");
+        return true;
+    }
+
+    /**
+     * #####    #####
+     * #####    #####
+     * #TH## -> #TH##
+     * #####    #####
+     * #####    #####
+     */
+    private static boolean testNoMove2() {
+        int[] head = {0, 0};
+        int[] tail = {1, 0};
+
+        Day09.tailFollowHead(head, tail);
+
+        int[] expectedAnswer = {1, 0};
+        if (tail[0] != expectedAnswer[0] || tail[1] != expectedAnswer[1]) {
+            System.err.println(CLASSNAME + " testNoMove2 KO | given [" + tail[0] + "," + tail[1] + "] expected [" + expectedAnswer[0] + "," + expectedAnswer[1] + "]");
+            return false;
+        }
+        System.out.println(CLASSNAME + " testNoMove2 OK");
         return true;
     }
 }
