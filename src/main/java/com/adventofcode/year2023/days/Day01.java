dca2184d4a29f4e632fe7b151f952d5ca977ee84
Arthur Jamain <a.jamain.pro@gmail.com>
2023-12-05 13:33:24 +0100

    [2023][Day1] Answer 2 & Tests

--- src/main/java/com/adventofcode/year2023/days/Day01.java
+++ src/main/java/com/adventofcode/year2023/days/Day01.java
@@ -11,6 +11,15 @@ public class Day01 {
 
     private static final String FILE_PATH = "src/main/resources/year2023/input-" + MethodHandles.lookup().lookupClass().getSimpleName().toLowerCase();
 
+    private static final String ONE = "one";
+    private static final String TWO = "two";
+    private static final String THREE = "three";
+    private static final String FOUR = "four";
+    private static final String FIVE = "five";
+    private static final String SIX = "six";
+    private static final String SEVEN = "seven";
+    private static final String EIGHT = "eight";
+    private static final String NINE = "nine";
 
     public static Integer answer1() {
 
@@ -22,15 +31,15 @@ public class Day01 {
             final String line = sc.nextLine();
 
             Matcher matcherFirst = Pattern.compile("^\\D*(?<first>\\d)").matcher(line);
-            Matcher matcherLast = Pattern.compile("(?<first>\\d)\\D*$").matcher(line);
+            Matcher matcherLast = Pattern.compile("(?<last>\\d)\\D*$").matcher(line);
 
             matcherFirst.find();
             String i = matcherFirst.group("first");
 
             matcherLast.find();
-            String j = matcherLast.group("first");
+            String j = matcherLast.group("last");
 
-            total += Integer.parseInt(i+j);
+            total += Integer.parseInt(i + j);
 
         }
 
@@ -41,6 +50,39 @@ public class Day01 {
 
         Scanner sc = AdventOfCodeUtils.getScanner(FILE_PATH);
 
-        return 0;
+        int total = 0;
+
+        while (sc.hasNextLine()) {
+            final String line = sc.nextLine();
+
+            Matcher matcherFirst = Pattern.compile("^.*?(?<first>\\d|(one)|(two)|(three)|(four)|(five)|(six)|(seven)|(eight)|(nine)).*").matcher(line);
+            Matcher matcherLast = Pattern.compile(".*(?<last>\\d|(one)|(two)|(three)|(four)|(five)|(six)|(seven)|(eight)|(nine)).*$").matcher(line);
+
+            matcherFirst.find();
+            String i = convert(matcherFirst.group("first"));
+
+            matcherLast.find();
+            String j = convert(matcherLast.group("last"));
+
+            total += Integer.parseInt(i + j);
+
+        }
+
+        return total;
+    }
+
+    private static String convert(String entry) {
+        return switch (entry) {
+            case ONE -> "1";
+            case TWO -> "2";
+            case THREE -> "3";
+            case FOUR -> "4";
+            case FIVE -> "5";
+            case SIX -> "6";
+            case SEVEN -> "7";
+            case EIGHT -> "8";
+            case NINE -> "9";
+            default -> entry;
+        };
     }
 }
--- src/test/java/com/adventofcode/year2023/days/Day01Test.java
+++ src/test/java/com/adventofcode/year2023/days/Day01Test.java
@@ -30,7 +30,7 @@ public class Day01Test extends DayTest {
 
     private static boolean testAnswer2() {
         final Integer answer = Day01.answer2();
-        int expectedAnswer = 0;
+        int expectedAnswer = 54676;
         if (answer != expectedAnswer) {
             System.err.println(CLASSNAME + " answer 2 KO | given " + answer + " expected " + expectedAnswer);
             return false;
